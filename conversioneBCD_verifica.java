168a86de342ea4c15e89b0c5b0bc3285ab5424f6
Alessandro Allegrucci <Alessandro Allegrucci>
2024-04-09 11:25:17 +0200

    primo commit

---
+++ conversioneBCD_verifica.java
@@ -0,0 +1,15 @@
+package it.edu.iisgubbio.github;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class conversioneBCD_verifica extends Application {
+	
+	public void start(Stage finestra) {
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
